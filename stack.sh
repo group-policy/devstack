6847e58332fb2e08e2a47d4fb3cd5fe992672d60
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-03-25 22:45:20 -0700

    Branch for GBP gate
    Conflicts:
    	local.conf
    	stack.sh

--- stack.sh
+++ stack.sh
@@ -753,7 +753,6 @@ if is_service_enabled neutron nova horizon; then
 fi
 if is_service_enabled heat horizon; then
     install_heatclient
-    install_gbpheat
 fi
 
 # Install middleware
@@ -796,7 +795,7 @@ fi
 
 if is_service_enabled neutron; then
     # Network service
-    install_gbpservice
+    #install_gbpservice
     stack_install_service neutron
     install_neutron_third_party
 fi
@@ -814,7 +813,6 @@ if is_service_enabled horizon; then
     # dashboard
     stack_install_service horizon
     configure_horizon
-    install_gbpui
 fi
 
 if is_service_enabled ceilometer; then
