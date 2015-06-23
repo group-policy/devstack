8ffe21cccb13306b3260cc71d65e66edc1874ead
Magesh GV <mageshgv@gmail.com>
2015-06-23 17:04:12 +0530

    Add delete service profile to gbp service exercise, pin gbpclient dependency to the patch that introduces service profile

--- exercises/gbp_servicechain.sh
+++ exercises/gbp_servicechain.sh
@@ -43,11 +43,11 @@ function confirm_server_active {
     fi
 }
 
-gbp service-profile-create --vendor default --insertion-mode l3 --servicetype FIREWALL fw_profile
-gbp service-profile-create --vendor default --insertion-mode l3 --servicetype LOADBALANCER lb_profile
+gbp service-profile-create --vendor default --insertion-mode l3 --servicetype FIREWALL fw-profile
+gbp service-profile-create --vendor default --insertion-mode l3 --servicetype LOADBALANCER lb-profile
 
-gbp  servicechain-node-create loadbalancer-node --template-file $TOP_DIR/gbp-templates/firewall-lb-servicechain/lb.template --service-profile lb_profile
-gbp  servicechain-node-create firewall-node --template-file $TOP_DIR/gbp-templates/firewall-lb-servicechain/fw.template  --service-profile fw_profile
+gbp  servicechain-node-create loadbalancer-node --template-file $TOP_DIR/gbp-templates/firewall-lb-servicechain/lb.template --service-profile lb-profile
+gbp  servicechain-node-create firewall-node --template-file $TOP_DIR/gbp-templates/firewall-lb-servicechain/fw.template  --service-profile fw-profile
 
 gbp servicechain-spec-create firewall-loadbalancer-spec --description spec --nodes "firewall-node loadbalancer-node"
 
@@ -145,6 +145,9 @@ gbp servicechain-spec-delete firewall-loadbalancer-spec
 
 gbp  servicechain-node-delete loadbalancer-node
 gbp  servicechain-node-delete firewall-node
+
+gbp service-profile-delete lb-profile
+gbp service-profile-delete fw-profile
 
 set +o xtrace
 echo "*********************************************************************"
